e8e05c325f55610356ab99c55b4aec91c3d6e50f
Robert <robertogarciadevelopment@gmail.com>
2022-07-03 19:05:01 +0200

    [_]: Add sha256 and sha512 native hash methods for iOS

---
+++ ios/RnCryptoTests/KeyDerivationTest.swift
@@ -0,0 +1,29 @@
+//
+//  KeyDerivation.swift
+//  RnCryptoTests
+//
+//  Created by Robert on 1/7/22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+import Foundation
+import XCTest
+@testable import RnCrypto
+import IDZSwiftCommonCrypto
+class KeyDerivation: XCTestCase {
+    let sut = RnCryptoKeyDerivation()
+    let utils = RnCryptoUtils()
+
+    func testPbkdf2() throws {
+        let password = String("testpassword")
+        let salt = String("testsalt")
+        let rounds = 2048
+        let derivedKeyLength = 64
+        let result = sut.pbkdf2(password: password, salt: salt, rounds: rounds, derivedKeyLength: derivedKeyLength)
+        
+        XCTAssertEqual(utils.bytesToHexString(result), "23c999c8753e1deec3aa8638cd4407f241b0184ad35f7b71be9af5266e6ad31c8025a88e1fe92a03a3815fa35d1b823294f6b4ba79619d52b911f215fe56ae24")
+        
+    }
+
+    
+
+}
--- ios/RnCryptoUtils.swift
+++ ios/RnCryptoUtils.swift
@@ -12,8 +12,23 @@ class RnCryptoUtils {
     public func hexStringToBytes(_ hexString: String) -> [UInt8] {
         return arrayFrom(hexString: hexString)
     }
+    
+    public func bytesToHexString(_ bytes: [UInt8]) -> String {
+        let format = "%02hhx"
+        return bytes.map { String(format: format, $0) }.joined()
+    }
 }
 
+extension StringProtocol {
+    var hex: [UInt8] {
+        var startIndex = self.startIndex
+        return (0..<count/2).compactMap { _ in
+            let endIndex = index(after: startIndex)
+            defer { startIndex = index(after: endIndex) }
+            return UInt8(self[startIndex...endIndex], radix: 16)
+        }
+    }
+}
 
 enum RnCryptoError: Error {
     case badIv
